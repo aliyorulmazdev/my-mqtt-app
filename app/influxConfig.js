438234feec3c3130bae25ecae9790a7196f3cb09
Ali Yorulmaz <kbr.ercenk@gmail.com>
2024-01-05 15:47:59 +0300

    test

---
+++ app/influxConfig.js
@@ -0,0 +1,9 @@
+// InfluxDBConfig.js
+const InfluxDBConfig = {
+  url: 'https://us-east-1-1.aws.cloud2.influxdata.com',
+  token: 'iHTPPUYisX1y9HsghPoAsBTnlqMYIysWo7P1rBsZROBIahjYtv9BfP3CE1NGWHiLi7w6rtJ7aV6K_tqAxTRt6w==',
+  bucket: 'sensordata',
+  org: 'aliyrlmz'
+};
+
+export { InfluxDBConfig };
--- app/page.js
+++ app/page.js
@@ -1,36 +1,50 @@
-"use client"
-import React, { useState, useEffect } from 'react';
-import { Chart } from 'react-google-charts';
-import { db } from './firebaseConfig'; // db bağlantısını ekleyin
-import { collection, getDocs } from 'firebase/firestore';
+"use client";
+import React, { useState, useEffect } from "react";
+import { Chart } from "react-google-charts";
+import { InfluxDB } from "@influxdata/influxdb-client";
+import { InfluxDBConfig } from './influxConfig';
+import ReactDOM from 'react-dom';
+
+const influx = new InfluxDB({
+  url: InfluxDBConfig.url,
+  token: InfluxDBConfig.token,
+});
+const queryApi = influx.getQueryApi(InfluxDBConfig.org);
 
 const Home = () => {
   const [temperatureData, setTemperatureData] = useState([]);
 
   const sortDataByDate = (data) => {
-    // Sort data based on date in descending order
-    return data.sort((a, b) => b.date.seconds - a.date.seconds);
+    return data.sort(
+      (a, b) => new Date(b.date).getTime() - new Date(a.date).getTime()
+    );
   };
 
   useEffect(() => {
     const fetchData = async () => {
       try {
-        // Firestore'dan temperatureData koleksiyonundaki verileri al
-        const querySnapshot = await getDocs(collection(db, 'temperatureData'));
-        
+        const fluxQuery =
+          'from(bucket:"sensordata") |> range(start: -30d) |> filter(fn: (r) => r._measurement == "temperature_data")';
+
+        // Execute InfluxDB query
+        const result = await queryApi.collectRows(fluxQuery);
+
         // Alınan veriyi bir diziye dönüştür
-        const data = querySnapshot.docs.map((doc) => doc.data());
+        const data = result.map((row) => ({
+          date: new Date(row._time),
+          value: row._value,
+        }));
 
         // Sort data by date
         const sortedData = sortDataByDate(data);
 
-        // Take the first 5 elements
+        // Take the first 10 elements
         const slicedData = sortedData.slice(0, 10);
 
         // State'i güncelle
         setTemperatureData(slicedData);
       } catch (error) {
-        console.error('Error fetching data:', error);
+        console.error("Error fetching data from InfluxDB:", error);
       }
     };
 
@@ -45,30 +59,49 @@ const Home = () => {
   }, []);
 
   const chartData = [
-    ['Time', 'Temperature'],
+    ["Time", "Temperature"],
     ...temperatureData.map((data) => [
-      new Date(data.date.seconds * 1000).toLocaleTimeString(),
+      data.date.toLocaleTimeString(),
       data.value,
     ]),
   ];
 
+  useEffect(() => {
+    const handleWheel = (e) => {
+      e.preventDefault();
+    };
+
+    const chartContainer = document.getElementById('chart-container');
+    chartContainer.addEventListener('wheel', handleWheel, { passive: false });
+
+    return () => {
+      chartContainer.removeEventListener('wheel', handleWheel);
+    };
+  }, []);
+
   return (
-    <div className="container mx-auto p-8">
-      <h1 className="text-3xl font-bold mb-8">1Tik.Net Live Temperature Data</h1>
-      <Chart
-        chartType="LineChart"
-        width="100%"
-        height="400px"
-        data={chartData}
-        options={{
-          hAxis: {
-            title: 'Time',
-          },
-          vAxis: {
-            title: 'Temperature',
-          },
-        }}
-      />
+    <div id="chart-container" className="container mx-auto p-8">
+      <h1 className="text-3xl font-bold mb-8">
+        InfluxDB Live Temperature Data
+      </h1>
+      {temperatureData.length > 0 ? (
+        <Chart
+          chartType="LineChart"
+          width="100%"
+          height="400px"
+          data={chartData}
+          options={{
+            hAxis: {
+              title: "Time",
+            },
+            vAxis: {
+              title: "Temperature",
+            },
+          }}
+        />
+      ) : (
+        <p>Loading data...</p>
+      )}
     </div>
   );
 };
